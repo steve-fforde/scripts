a1a6f2cebc015ff0f51898392ea87dbe4d915836
Steven Cotton <steven.cotton@ros.gov.uk>
2021-11-22 10:56:57 +0000

    wip

--- csm.sh
+++ csm.sh
@@ -1,12 +1,20 @@
-echo "Chayote Script Manager"
-
 if [ "$1" = "--help" ]; then
-  echo "csm setup-aws-cdk"
+  echo "usage: csm  [--help] <command> [<args>]
+
+These are common CSM commands used in various situations:
+
+For use on AWS boxes:
+   setup-aws-cdk      Install CDK on an EC2 box."
+   
+  exit 0
 fi
+
 (
   code=$(curl -s -o /dev/null -w "%{http_code}" "https://raw.githubusercontent.com/steve-fforde/scripts/main/$1.sh")
-  echo "~~~ $code ~~~"
-  curl -s "https://raw.githubusercontent.com/steve-fforde/scripts/main/$1.sh" -o /tmp/$1.sh
-  bash /tmp/$1.sh ${@:2} 
+  if [ code = 200 ]; then
+    curl -s "https://raw.githubusercontent.com/steve-fforde/scripts/main/$1.sh" -o /tmp/$1.sh
+    bash /tmp/$1.sh ${@:2} 
+  else
+    echo "csm: '$1' is not a csm command. See 'csm --help'."
 )
 rm -f /tmp/$1.sh
