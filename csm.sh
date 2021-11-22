37eeec095969d88c372fe3a045a331b098877509
Steven Cotton <steven.cotton@ros.gov.uk>
2021-11-22 10:49:25 +0000

    wip

--- csm.sh
+++ csm.sh
@@ -4,7 +4,7 @@ if [ "$1" = "--help" ]; then
   echo "csm setup-aws-cdk"
 fi
 (
-  curl -s "https://raw.githubusercontent.com/steve-fforde/scripts/main/csm.sh" -o /tmp/$1.sh
+  curl -s "https://raw.githubusercontent.com/steve-fforde/scripts/main/$1.sh" -o /tmp/$1.sh
   bash /tmp/$1.sh ${@:2} 
 )
 rm -f /tmp/$1.sh
