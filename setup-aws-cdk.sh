41530e4ca212d6c48a89ac0ac779b6b831f94bd4
Steven Cotton <steven.cotton@ros.gov.uk>
2021-11-22 07:40:10 +0000

    wip

--- setup-aws-cdk.sh
+++ setup-aws-cdk.sh
@@ -4,9 +4,10 @@ source <(curl -s "https://raw.githubusercontent.com/steve-fforde/scripts/main/ut
 
 h1 "Setting up AWS Cloud Development Kit"
 
-region=$(askWithDefault "Enter your region" "eu-west-2")
+region=$(askWithDefault "Enter your region" "$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | python3 -c 'import sys, json; print(json.load(sys.stdin)["region"])')")
 adfs_host=$(ask "Enter your ADFS Host")
 
+echo "$@"
 
 echo ""
 echo " ++++ $region ++++"
