5cc3038ba870c517918ecb298682ce7e7700391b
Steven Cotton <steven.cotton@ros.gov.uk>
2021-11-25 13:28:32 +0000

    wip

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo bash -c "echo '( curl -s \"https://raw.githubusercontent.com/steve-fforde/scripts/main/csm.sh\" -o /tmp/csm.sh && bash /tmp/csm.sh $@ ) ;  rm -f /tmp/csm.sh' > /usr/local/sbin/csm"
+sudo bash -c "echo '( curl -s \"https://raw.githubusercontent.com/steve-fforde/scripts/main/csm.sh\" -o /tmp/csm.sh && bash /tmp/csm.sh \$@ ) ;  rm -f /tmp/csm.sh' > /usr/local/sbin/csm"
 sudo chmod +x /usr/local/sbin/csm
 
 csm --version
