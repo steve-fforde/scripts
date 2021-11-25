ed2f8f17f2f0df714c877db9c3227516efc69b4e
Steven Cotton <steven.cotton@ros.gov.uk>
2021-11-25 13:10:39 +0000

    wip

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+if [ $EUID -ne 0 ]; then
+    echo "csm installer: you must run this command as sudo"
+    exit 1
+fi
+
 sudo bash -c "echo '( curl -s \"https://raw.githubusercontent.com/steve-fforde/scripts/main/csm.sh\" -o /tmp/csm.sh && bash /tmp/csm.sh $@ ) ;  rm -f /tmp/csm.sh' > /usr/local/sbin/csm"
 sudo chmod +x /usr/local/sbin/csm
 
 csm --version
-cms --help
+csm --help
--- src/aws-ec2-setup-lamp.sh
+++ src/aws-ec2-setup-lamp.sh
@@ -1,9 +1,27 @@
-sudo yum update -y
-sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-sudo yum install -y httpd mariadb-server
-sudo systemctl start httpd
-sudo systemctl enable httpd
-echo "<h1>Hello, world</h1>" > /var/www/index.html
-sudo usermod -a -G apache $(whoami)
-sudo chown apache:apache /var/www/index.html
-sudo chmod 660 /var/www/index.html
+#!/bin/bash
+
+local lineNo=1
+
+function try {
+  lineNo=$(( lineNo++ ))
+  bash -c "$1"
+  errCode=$?
+  if [ $errCode != 0 ]; then
+    echo "script error: line ${lineNo}. command '$1' failed with error ${errCode}."
+    exit $errCode
+  fi
+}
+
+try "echo 'hello'"
+try "echo 'hello'"
+try "exit 213"
+
+try "sudo yum update -y"
+try "sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2"
+try "sudo yum install -y httpd mariadb-server"
+try "sudo systemctl start httpd"
+try "sudo systemctl enable httpd"
+try "echo \"<h1>Hello, world</h1>\" > /var/www/index.html"
+try "sudo usermod -a -G apache $(whoami)"
+try "sudo chown apache:apache /var/www/index.html"
+try "sudo chmod 660 /var/www/index.html"
