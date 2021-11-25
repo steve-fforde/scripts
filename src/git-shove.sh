85fd8289e3cf6f880e06cc8db79920ce3d5a0037
Steven Cotton <steven.cotton@ros.gov.uk>
2021-11-25 15:55:59 +0000

    work in progress

--- src/git-shove.sh
+++ src/git-shove.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 memo=$1
-if [ "$memo" -eq "" ]; then
+if [ "$memo" == "" ]; then
   memo="work in progress"
 fi
 
 git add . || exit 1
-git commit -m "wip" || exit 2
+git commit -m "$memo" || exit 2
 git push || exit 3
