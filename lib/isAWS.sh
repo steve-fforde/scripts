7b87b63b5444b0c39ae48ee2a9542850e37dbafa
Steve Cotton <steve.m.cotton@gmail.com>
2021-11-26 15:52:18 +0000

    Update isAWS.sh

--- lib/isAWS.sh
+++ lib/isAWS.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function isAWS {
+  # Checking if the AWS Metadata server is reachable
   code=$(curl -s -o /dev/null -w "%{http_code}" 169.254.169.254)
-  echo "AWS Metadata server responded with HTTP Status $code"
   if (( $code >= 200 && $code < 300 )); then
     return 0 # true
   else
