0e29c71d2dbfbb7c80f978fd734a2ba2632a8a49
Steve Cotton <steve.m.cotton@gmail.com>
2021-11-26 15:01:42 +0000

    Update aws-ec2-setup-lamp.sh

--- src/aws-ec2-setup-lamp.sh
+++ src/aws-ec2-setup-lamp.sh
@@ -2,10 +2,10 @@
 
 import isAWS
 
-# if (( ! isAWS )); then
-#     echo "csm aws-ec2-setup-lamp: You must run this script on an AWS EC2 server"
-#     exit 3
-# fi
+if (( ! isAWS )); then
+  echo "csm aws-ec2-setup-lamp: You must run this script on an AWS EC2 server"
+  exit 3
+fi
 
 function try {
   lineNo=$(( lineNo++ ))
