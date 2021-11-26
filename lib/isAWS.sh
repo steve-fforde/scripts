dd99416f068385cdf62588291d607d865b6828d9
Steve Cotton <steve.m.cotton@gmail.com>
2021-11-26 14:26:39 +0000

    Update isAWS.sh

--- lib/isAWS.sh
+++ lib/isAWS.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 function isAWS {
-  return $(nc -v -z -w 3 169.254.169.254 80 2>/dev/null && echo 1 || echo 0)
+  #return $(nc -v -z -w 3 169.254.169.254 80 2>/dev/null && echo 1 || echo 0)
+  return 0
 }
