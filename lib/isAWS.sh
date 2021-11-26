5989d5c0de74b19a8125195b298d58e04abedc7e
Steve Cotton <steve.m.cotton@gmail.com>
2021-11-26 15:29:17 +0000

    Update isAWS.sh

--- lib/isAWS.sh
+++ lib/isAWS.sh
@@ -2,6 +2,7 @@
 
 function isAWS {
   code=$(curl -s -o /dev/null -w "%{http_code}" 169.254.169.254)
+  echo "AWS Reflector responded with HTTP Status $code"
   if (( $code >= 200 && $code < 300 )); then
     return 0 # true
   else
